b4695f808a318a44af6b84fbe610c2f0650369c8
鱼鱼蒸 <98036825+Yuyuzheng2499@users.noreply.github.com>
2025-11-05 16:40:34 +0800

    Change default cover and dark mode settings

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -25,7 +25,7 @@ const CONFIG = {
   HEXO_POST_LIST_COVER: true, // 列表显示文章封面
   HEXO_POST_LIST_COVER_HOVER_ENLARGE: false, // 列表鼠标悬停放大
 
-  HEXO_POST_LIST_COVER_DEFAULT: true, // 封面为空时用站点背景做默认封面
+  HEXO_POST_LIST_COVER_DEFAULT: false, // 封面为空时用站点背景做默认封面
   HEXO_POST_LIST_SUMMARY: true, // 文章摘要
   HEXO_POST_LIST_PREVIEW: false, // 读取文章预览
   HEXO_POST_LIST_IMG_CROSSOVER: true, // 博客列表图片左右交错
@@ -39,7 +39,7 @@ const CONFIG = {
   HEXO_WIDGET_ANALYTICS: false, // 显示统计卡
   HEXO_WIDGET_TO_TOP: true,
   HEXO_WIDGET_TO_COMMENT: true, // 跳到评论区
-  HEXO_WIDGET_DARK_MODE: true, // 夜间模式
+  HEXO_WIDGET_DARK_MODE: false, // 夜间模式
   HEXO_WIDGET_TOC: true, // 移动端悬浮目录
 
   HEXO_THEME_COLOR: '#928CEE' // 主题色配置（默认为 #928CEE）
